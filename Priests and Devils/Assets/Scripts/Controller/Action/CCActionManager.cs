02b5f78882ab4be4de4d439ff0164c8d28714fd7
Runner1014 <31625057+Runner1014@users.noreply.github.com>
2018-04-10 22:53:36 +0800

    Add files via upload

---
+++ Priests and Devils/Assets/Scripts/Controller/Action/CCActionManager.cs
@@ -0,0 +1,30 @@
+﻿using Mygame;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCActionManager : SSActionManager, ISSActionCallback
+{
+    public void MoveBoat(BoatController boat)
+    {
+        CCMoveToAction action = CCMoveToAction.GetSSAction(boat.GetDestination(), boat.speed);
+        this.RunAction(boat.GetGameobj(), action, this);
+    }
+
+    public void MoveCharacter(CharacterController characterCtrl, Vector3 destination)
+    {
+        Vector3 currentPos = characterCtrl.GetPos();
+        Vector3 middlePos = currentPos;
+        if (destination.y > currentPos.y)
+        {
+            middlePos.y = destination.y;
+        }
+        else
+        {
+            middlePos.x = destination.x;
+        }
+        SSAction action1 = CCMoveToAction.GetSSAction(middlePos, characterCtrl.speed);
+        SSAction action2 = CCMoveToAction.GetSSAction(destination, characterCtrl.speed);
+        SSAction seqAction = CCSequenceAction.GetSSAction(1, 0, new List<SSAction> { action1, action2 });
+        this.RunAction(characterCtrl.GetGameobj(), seqAction, this);
+    }
+}
